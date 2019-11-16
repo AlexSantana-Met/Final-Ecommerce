863820bb40ab840424c0c090f4acd4c29043582b
AlexSantana-Met <alejandro.sant12.12@gmail.com>
2019-11-15 21:51:55 -0600

    Primer commit

---
+++ Final-Ecommerce/Controllers/UsuariosController.cs
@@ -0,0 +1,109 @@
+﻿using Final_Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Ecommerce_Shop.Repository;
+using Final_Ecommerce.Models.DAL;
+using System.Threading.Tasks;
+
+namespace Final_Ecommerce.Controllers
+{
+    [Authorize]
+    public class UsuariosController : Controller
+    {
+        public GenericUnitToWork _unitOfWork = new GenericUnitToWork();
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(UsuarioModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.correo, Email = model.correo };
+                var result = await UserManager.CreateAsync(user, model.pass);
+                if (result.Succeeded)
+                {
+                    Usuarios newUser = new Usuarios();
+                    //List<Usuarios> lista = _unitOfWork.GetRepositoryInstance<Usuarios>().GetAllRecords().ToList();
+                    List<Usuarios> u = _unitOfWork.GetRepositoryInstance<Usuarios>().GetAllRecords().ToList();
+                    if (u.Count() != 0)
+                    {
+                        newUser.id = u.Last().id + 1;
+                    }
+                    newUser.correo = model.correo;
+                    newUser.username = model.username;
+                    newUser.role_id = 1;
+                    Session["usr"] = newUser;
+
+                    newUser.nombre = model.nombre;
+                    newUser.apellido_paterno = model.apellido_paterno;
+                    newUser.apellido_materno = model.apellido_materno;
+                    newUser.pass = model.pass;
+                    newUser.status = 1;
+                    newUser.telefono = model.telefono;
+
+                    newUser.fecha_nacimiento = model.fecha_nacimiento;
+                    _unitOfWork.GetRepositoryInstance<Usuarios>().Add(newUser);
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+                Session["usr"] = null;
+            }
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+    }
+}
---
+++ Final-Ecommerce/Repository/GenericRepository.cs
@@ -0,0 +1,121 @@
+﻿using Final_Ecommerce.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace Ecommerce_Shop.Repository
+{
+    public class GenericRepository<Entidad> : IRepository<Entidad> where Entidad : class
+    {
+        DbSet<Entidad> _dbSet;
+        private Entities _DBEntity;
+
+        public GenericRepository(Entities DBEntity)
+        {
+            _DBEntity = DBEntity;
+            _dbSet = _DBEntity.Set<Entidad>();
+        }
+
+        public void Add(Entidad entity)
+        {
+            _dbSet.Add(entity);
+            _DBEntity.SaveChanges();
+        }
+
+        public int GetAllRecordCount()
+        {
+            return _dbSet.Count();
+        }
+
+        public IEnumerable<Entidad> GetAllRecords()
+        {
+            return _dbSet.ToList();
+        }
+
+        public IQueryable<Entidad> GetAllRecordsQueryable()
+        {
+            return _dbSet;
+        }
+
+        public Entidad GetFirstOrDefault(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public Entidad GetFirstOrDefaultByParameter(Expression<Func<Entidad, bool>> wherePredict)
+        {
+            return _dbSet.Where(wherePredict).FirstOrDefault();
+        }
+
+        public IEnumerable<Entidad> GetListParameter(Expression<Func<Entidad, bool>> wherePredict)
+        {
+            return _dbSet.Where(wherePredict).ToList();
+        }
+
+        public IEnumerable<Entidad> GetResultBySqlProcedure(string query, params object[] parameters)
+        {
+            if (parameters != null)
+            {
+                return _DBEntity.Database.SqlQuery<Entidad>(query, parameters).ToList();
+            }
+            else
+                return _DBEntity.Database.SqlQuery<Entidad>(query).ToList();
+        }
+
+        public void InactiveAndDeleteByWhereClause(Expression<Func<Entidad, bool>> wherePredict, Action<Entidad> ForEachPredict)
+        {
+            _dbSet.Where(wherePredict).ToList().ForEach(ForEachPredict);
+        }
+
+        public void Remove(Entidad entidad)
+        {
+            if (_DBEntity.Entry(entidad).State == EntityState.Detached)
+                _dbSet.Attach(entidad);
+            _dbSet.Remove(entidad);
+        }
+
+        public void RemoveByWhereClause(Expression<Func<Entidad, bool>> wherePredict)
+        {
+            Entidad entity = _dbSet.Where(wherePredict).FirstOrDefault();
+            Remove(entity);
+        }
+
+        public void RemoveRangeByWhereClause(Expression<Func<Entidad, bool>> wherePredict)
+        {
+            List<Entidad> entity = _dbSet.Where(wherePredict).ToList();
+            foreach (var ent in entity)
+            {
+                Remove(ent);
+            }
+        }
+
+        public void Update(Entidad entity)
+        {
+            _dbSet.Attach(entity);
+            _DBEntity.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void UpdateByWhere(Expression<Func<Entidad, bool>> wherePredict, Action<Entidad> ForEachPredict)
+        {
+            _dbSet.Where(wherePredict).ToList().ForEach(ForEachPredict);
+        }
+
+
+        public IEnumerable<Entidad> GetRecordsToShow(int PageNo, int PageSize, int CurrentPage, Expression<Func<Entidad, bool>> wherePredict, Expression<Func<Entidad, int>> orderByPredict)
+        {
+            if (wherePredict != null)
+            {
+                return _dbSet.OrderBy(orderByPredict).Where(wherePredict).ToList();
+            }
+            else
+            {
+                return _dbSet.OrderBy(orderByPredict).ToList();
+            }
+
+        }
+
+    }
+}
